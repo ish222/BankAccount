3a0e64907bf4a57587b42b416b5d2c2df117a288
ish222 <68574796+ish222@users.noreply.github.com>
2020-11-23 23:03:10 +0000

    Debugged issues regarding class and some functions. Program is now fully functional for all of the functions implemented so far.

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 import sqlite3
 
 
-def saveData(fname, lname, balance, creditcard, id, password):  # Adds a row of new user information when they register
+def initSaveData(fname, lname, balance, creditcard, id, password):  # Adds a row of new user information when they register
     conn = sqlite3.connect('clients.db')
-
     c = conn.cursor()
 
     # c.execute('''CREATE TABLE clients
@@ -13,13 +12,22 @@ def saveData(fname, lname, balance, creditcard, id, password):  # Adds a row of 
               {'fname': fname, 'lname': lname, 'balance': balance,'creditcard': creditcard, 'id': id, 'password': password})
 
     conn.commit()
+    conn.close()
 
+
+def saveData(balance, id):
+    conn = sqlite3.connect('clients.db')
+    c = conn.cursor()
+
+    c.execute("UPDATE clients SET balance = :balance WHERE id = :id",
+              {'balance': balance, 'id': id})
+
+    conn.commit()
     conn.close()
 
 
 def getIDs():  # Gets a list of all the IDs of users for login purposes
     conn = sqlite3.connect('clients.db')
-
     c = conn.cursor()
     
     id_list = [id[0] for id in c.execute("SELECT id FROM clients")]
@@ -30,7 +38,6 @@ def getIDs():  # Gets a list of all the IDs of users for login purposes
 
 def getPasswords():  # Gets a list of all the passwords
     conn = sqlite3.connect('clients.db')
-
     c = conn.cursor()
 
     pass_list = [passw[0] for passw in c.execute("SELECT password FROM clients")]
@@ -39,37 +46,8 @@ def getPasswords():  # Gets a list of all the passwords
     return pass_list
 
 
-def idPassDict(id_list, pass_list):  # Use this function to create a dictionary of id and password for each user for login purposes
-    pass
-
-
-def getCC(idin):
-    conn = sqlite3.connect('clients.db')
-
-    c = conn.cursor()
-
-    c.execute("SELECT creditcard FROM clients WHERE id=:id", {'id': idin})
-    ccard = c.fetchone()
-
-    conn.close()
-    return ccard
-
-
-def getBalance(idin):
-    conn = sqlite3.connect('clients.db')
-
-    c = conn.cursor()
-
-    c.execute("SELECT balance FROM clients WHERE id=:id", {'id': idin})
-    balance = c.fetchone()
-
-    conn.close()
-    return balance
-
-
 def getData(idin):  # Gets row of data for specific user once they login
     conn = sqlite3.connect('clients.db')
-
     c = conn.cursor()
 
     c.execute("SELECT * FROM clients WHERE id=:id", {'id': idin})
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Todo:
-Fix class
-Find a way to make a global instance of class
+Fix class DONE
+Find a way to make a global instance of class DONE
 Complete service functions
 Add new services.
 """
@@ -12,30 +12,36 @@ from database import *
 
 
 class BankAccount:
-    def __init__(self, id, fname, lname, init_deposit):
-        if self.id == "":
-            self.id = self.newid()
-            self.fname = fname.lower()
-            self.lname = lname.lower()
-            self.credit_card = self.newcreditCard()
-            self.password = self.newpassword()
-            self.balance += init_deposit
-        else:
-            data = getData(id)  # Data is ('john', 'smith', 2500, 2369149726456860, 'johnJYSvY', 'I=HO1G)y')
-            self.fname = data[0]
-            self.lname = data[1]
-            self.balance = data[2]
-            self.credit_card = data[3]
-            self.id = id
-            self.password = data[5]
+    def __init__(self, id):
+        self.data = getData(id)  # Data is ('john', 'smith', 2500, 2369149726456860, 'johnJYSvY', 'I=HO1G)y')
+        self.fname = self.data[0]
+        self.lname = self.data[1]
+        self.balance = self.data[2]
+        self.credit_card = self.data[3]
+        self.id = id
+        self.password = self.data[5]
 
     def deposit(self, amount):
         self.balance += amount
-        return self.balance
 
     def withdraw(self, amount):
         self.balance -= amount
-        return self.balance
+
+    def saveToDatabase(self):
+        return self.fname, self.lname, self.balance, self.credit_card, self.id, self.password
+
+    def getFromDatabase(self):
+        pass
+
+
+class NewAccount:
+    def __init__(self, fname, lname, init_deposit):
+        self.fname = fname.lower()
+        self.lname = lname.lower()
+        self.balance = init_deposit
+        self.credit_card = self.newcreditCard()
+        self.password = self.newpassword()
+        self.id = self.newid()
 
     def newcreditCard(self):
         num_list = string.digits
@@ -45,7 +51,7 @@ class BankAccount:
     def newid(self):
         char_list_id = string.ascii_letters + string.digits
         id_rand = ''.join(random.choice(char_list_id) for j in range(5))
-        self.id = self.fname.lower() + id_rand
+        self.id = self.fname + id_rand
         return self.id
 
     def newpassword(self):
@@ -56,29 +62,37 @@ class BankAccount:
     def saveToDatabase(self):
         return self.fname, self.lname, self.balance, self.credit_card, self.id, self.password
 
-    def getFromDatabase(self):
-        pass
+
+def instance(id):
+    """
+    Creates a global instance of the BankAccount class
+    :param id: User id
+    :return: None
+    """
+    global id_class
+    id_class = BankAccount(id)
 
 
 def newAcc():
     fname = input("As you are a new customer, you should open a new account. Please input your first name: ")
     lname = input("Please input your last name: ")
-    try:
-        initdep = int(input("Please input your initial deposit (input 0 for no deposit): "))
-    except Exception as e:
-        print("Invalid input!", e)
-        newAcc()
-    id_class = BankAccount("")
-    
-    print(f"Your initial balance is {id_class.balance}")
-    print(f"You new credit card number is: {id_class.creditCard()}")
-    print(f"The ID you will use to login to this service again is: {id_class.id()}")
-    print(f"Your password is {id_class.password()}. Please ensure you store it in a password manager.")
+    # try:
+    initdep = int(input("Please input your initial deposit (input 0 for no deposit): "))
+    # except Exception as e:
+    #     print("Invalid input!", e)
+    #     newAcc()
+    newid_class = NewAccount(fname, lname, initdep)
+    print(f"Your initial balance is {newid_class.balance}")
+    print(f"You new credit card number is: {newid_class.credit_card}")
+    print(f"The ID you will use to login to this service again is: {newid_class.id}")
+    print(f"Your password is {newid_class.password}. Please ensure you store it in a password manager.")
     with open('id.txt', 'a') as f:  # Saves the username and password data temporarily for testing purposes
-        f.write(id_class.id + " ")
-        f.write(id_class.password + "\n")
-    cdata = id_class.saveToDatabase()
-    saveData(cdata[0], cdata[1], cdata[2], cdata[3], cdata[4], cdata[5])
+        f.write(newid_class.id + " ")
+        f.write(newid_class.password + "\n")
+    cdata = newid_class.saveToDatabase()
+    initSaveData(cdata[0], cdata[1], cdata[2], cdata[3], cdata[4], cdata[5])
+    print("Please restart the program and login with your new credentials!")
+    quit()
 
 
 def login():
@@ -91,7 +105,10 @@ def login():
     if u_name in id_dict:
         password = input("Please input your password: ")
         if id_dict[u_name] == password:
-            main(u_name)
+            instance(u_name)
+            name = id_class.fname + " " + id_class.lname
+            print(f"Welcome {name.capitalize()}!\n\nYou've logged in!\n")
+            main()
         else:
             print("Wrong password, please try again!")
             login()
@@ -100,6 +117,7 @@ def login():
     else:
         print("Invalid username, please try again!")
         login()
+
 
 
 def firstFunction():
@@ -118,8 +136,9 @@ def switch1():
     Deposit
     :return: New balance
     """
-    deposit = input("How much would you like to deposit?")
+    deposit = int(input("How much would you like to deposit?"))
     id_class.deposit(deposit)
+    main()
     
 
 
@@ -128,8 +147,13 @@ def switch2():
     Withdraw
     :return: New balance
     """
-
-    print("This is switch 2")
+    withdraw = int(input("How much would you like to withdraw?"))
+    if id_class.balance > withdraw:
+        id_class.withdraw(withdraw)
+        print(f"Your new balance is {id_class.balance}")
+    else:
+        print(f"You have insufficient funds for this transaction. Your current balance is {id_class.balance}")
+    main()
 
 
 def switch3():
@@ -137,8 +161,8 @@ def switch3():
     Get Credit card number
     :return: Credit card number
     """
-
-    print("This is switch 3")
+    print(f"Your credit card number is: {id_class.credit_card}")
+    main()
 
 
 def switch4():
@@ -147,7 +171,8 @@ def switch4():
     :return:call function of service
     """
 
-    print("This is switch 4")
+    print("Services are still in progress! Please come back soon.")
+    main()
 
 
 def switch5():
@@ -155,18 +180,20 @@ def switch5():
     Log out
     :return: None
     """
+    saveData(id_class.balance, id_class.id)
     print("Thank you for using this program!")
     quit()
 
 
-def main(id):
-    # data = getData(id)  # Data is ('john', 'smith', 2500, 2369149726456860, 'johnJYSvY', 'I=HO1G)y')
-    id_class = BankAccount(id, "", "", 0)
-    name = id_class.fname
-    print(f"Welcome {name.capitalize()}!\n\nYou've logged in!\n")
+def main():
+    print(f"Your current balance is {id_class.balance}")
     to_do = input("What would you like to do? \nPress 1 to deposit.\nPress 2 to withdraw.\nPress 3 to get your credit card number.\nPress 4 for a service.\nPress 5 to log out.\n")
-
     def toDo(switch):
+        """
+        Functionality of a switch statement, didnt want to use too many conditional statements
+        :param switch: switch number
+        :return: None
+        """
         func = "switch" + str(switch)
         eval(func + "()")
     toDo(to_do)
