a8f70d828a2902eb73fa9554619b280075f8b27c
ish222 <68574796+ish222@users.noreply.github.com>
2020-11-24 21:57:48 +0000

    Added new features. Added loan, savings account and mortgage functionality with interest implemented using the difference in dates between logins. Need to finish other remaining features.

--- database.py
+++ database.py
@@ -2,14 +2,31 @@ import sqlite3
 
 
 def initSaveData(fname, lname, balance, creditcard, id, password):  # Adds a row of new user information when they register
+    """
+    # c.execute('''CREATE TABLE clients
+    #             (fname text, lname text, balance integer, creditcard integer, id text, password text)''')
+    The command above was used to create the table for the base data of the users.
+    """
     conn = sqlite3.connect('clients.db')
     c = conn.cursor()
 
-    # c.execute('''CREATE TABLE clients
-    #             (fname text, lname text, balance integer, creditcard integer, id text, password text)''')
-
     c.execute("INSERT INTO clients VALUES (:fname, :lname, :balance, :creditcard, :id, :password)",
               {'fname': fname, 'lname': lname, 'balance': balance,'creditcard': creditcard, 'id': id, 'password': password})
+
+    conn.commit()
+    conn.close()
+
+
+def secondaryFuncData(id, loan, mortgage, savingsacc, date):
+    """
+    c.execute('''CREATE TABLE secData
+                     (id text, loan integer, mortgage integer, savingsacc integer, date integer)''')
+    """
+    conn = sqlite3.connect('clients.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO secData VALUES (:id, :loan, :mortgage, :savingsacc, :curdate)",
+              {'id': id, 'loan': loan, 'mortgage': mortgage, 'savingsacc': savingsacc, 'curdate': date})
 
     conn.commit()
     conn.close()
@@ -21,6 +38,17 @@ def saveData(balance, id):
 
     c.execute("UPDATE clients SET balance = :balance WHERE id = :id",
               {'balance': balance, 'id': id})
+
+    conn.commit()
+    conn.close()
+
+
+def saveSecondaryData(id, loan, mortgage, savingsacc, date):
+    conn = sqlite3.connect('clients.db')
+    c = conn.cursor()
+
+    c.execute("UPDATE secData SET loan = :loan, mortgage = :mortgage, savingsacc = :savingsacc, ldate = :curdate WHERE id = :id",
+              {'loan': loan, 'mortgage': mortgage, 'savingsacc': savingsacc, 'id': id, 'curdate': date})
 
     conn.commit()
     conn.close()
@@ -55,3 +83,25 @@ def getData(idin):  # Gets row of data for specific user once they login
 
     conn.close()
     return data
+
+
+def getSecondaryData(idin):  # Gets row of their secondary data for specific user once they login
+    conn = sqlite3.connect('clients.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM secData WHERE id=:id", {'id': idin})
+    data = c.fetchone()
+
+    conn.close()
+    return data
+
+
+# def alterTable():
+#     conn = sqlite3.connect('clients.db')
+#     c = conn.cursor()
+#
+#     c.execute("DELETE FROM clients")
+#
+#     conn.commit()
+#     conn.close()
+# alterTable()
--- main.py
+++ main.py
@@ -9,17 +9,34 @@ Add new services.
 import string
 import random
 from database import *
+from datetime import date
 
+curDate = date.today()
+intCurDate = int(curDate.strftime('%Y%m%d'))
 
 class BankAccount:
     def __init__(self, id):
         self.data = getData(id)  # Data is ('john', 'smith', 2500, 2369149726456860, 'johnJYSvY', 'I=HO1G)y')
+        self.secData = getSecondaryData(id)
         self.fname = self.data[0]
         self.lname = self.data[1]
         self.balance = self.data[2]
         self.credit_card = self.data[3]
         self.id = id
         self.password = self.data[5]
+        self.loan = self.secData[1]
+        self.mortgage = self.secData[2]
+        self.savingsacc = self.secData[3]
+        self.LastDate = str(self.secData[4])
+        self.lastDate = date(year=int(self.LastDate[0:4]), month=int(self.LastDate[4:6]), day=int(self.LastDate[6:8]))
+        self.dateDelta = (curDate - self.lastDate).days
+        self.interestRate = 0
+        if self.loan != 0:
+            self.loanInterest()
+        if self.savingsacc != 0:
+            self.savingsInterest()
+        if self.mortgage != 0:
+            self.mortgageInterest()
 
     def deposit(self, amount):
         self.balance += amount
@@ -27,11 +44,20 @@ class BankAccount:
     def withdraw(self, amount):
         self.balance -= amount
 
-    def saveToDatabase(self):
-        return self.fname, self.lname, self.balance, self.credit_card, self.id, self.password
+    def loanInterest(self):
+        if self.loan != 0:
+            self.interestRate = float(0.15 / 365)
+            self.loan *= ((1 + self.interestRate)*self.dateDelta)
 
-    def getFromDatabase(self):
-        pass
+    def savingsInterest(self):
+        if self.savingsacc != 0:
+            self.interestRate = float(0.02 / 365)
+            self.savingsacc *= ((1 + self.interestRate)*self.dateDelta)
+
+    def mortgageInterest(self):
+        if self.mortgage != 0:
+            self.interestRate = float(0.0354 / 365)
+            self.mortgage *= ((1 + self.interestRate)*self.dateDelta)
 
 
 class NewAccount:
@@ -42,6 +68,13 @@ class NewAccount:
         self.credit_card = self.newcreditCard()
         self.password = self.newpassword()
         self.id = self.newid()
+        self.loan = 0
+        self.mortgage = 0
+        self.savingsacc = 0
+        self.date = intCurDate
+        initSaveData(self.fname, self.lname, self.balance, self.credit_card, self.id, self.password)
+        secondaryFuncData(self.id, self.loan, self.mortgage, self.savingsacc, self.date)
+
 
     def newcreditCard(self):
         num_list = string.digits
@@ -59,8 +92,99 @@ class NewAccount:
         self.password = ''.join(random.choice(char_list_pass) for k in range(8))
         return self.password
 
-    def saveToDatabase(self):
-        return self.fname, self.lname, self.balance, self.credit_card, self.id, self.password
+
+class InitSwitches:
+    @staticmethod
+    def switch1():
+        """
+        Deposit
+        :return: New balance
+        """
+        deposit = int(input("How much would you like to deposit?"))
+        id_class.deposit(deposit)
+        main()
+
+    @staticmethod
+    def switch2():
+        """
+        Withdraw
+        :return: New balance
+        """
+        withdraw = int(input("How much would you like to withdraw?"))
+        if id_class.balance > withdraw:
+            id_class.withdraw(withdraw)
+            print(f"Your new balance is {id_class.balance}")
+        else:
+            print(f"You have insufficient funds for this transaction. Your current balance is {id_class.balance}")
+        main()
+
+    @staticmethod
+    def switch3():
+        """
+        Get Credit card number
+        :return: Credit card number
+        """
+        print(f"Your credit card number is: {id_class.credit_card}")
+        main()
+
+    @staticmethod
+    def switch4():
+        """
+        Choose service
+        :return:call function of service
+        """
+        to_do = int(input(
+            "What would you like to do? \nPress 1 to request or repay a loan.\nPress 2 to request or repay a mortgage.\nPress 3 to deposit to or withdraw from your savings account.\n"
+            "Press 4 to report a fraud.\nPress 5 to go back.\n"))
+        if to_do == 1:
+            SecondarySwitches.loan()
+        print("Services are still in progress! Please come back soon.")
+        main()
+
+    @staticmethod
+    def switch5():
+        """
+        Log out
+        :return: None
+        """
+        saveData(id_class.balance, id_class.id)
+        saveSecondaryData(id_class.id, id_class.loan, id_class.mortgage, id_class.savingsacc, intCurDate)
+        print("Thank you for using this program!")
+        quit()
+
+
+class SecondarySwitches:
+    @staticmethod
+    def loan():
+        def addLoanFunc():
+            try:
+                addLoan = int(input("The interest rate we offer for loans is 15% APR. \nHow much would you like to take out: "))
+            except Exception as e:
+                print("Invalid input ", e)
+                SecondarySwitches.loan()
+            id_class.loan += addLoan
+
+        print(f"Your total current outstanding loan is: {id_class.loan}")
+        if id_class.loan == 0:
+            addLoanFunc()
+            InitSwitches.switch4()
+        to_do = input("Would you like to repay your loan or take out a new one? 'New' or 'Pay'")
+        if to_do.lower() == "new":
+            addLoanFunc()
+        elif to_do.lower() == "pay":
+            try:
+                payLoan = int(
+                    input("How much would you like to pay back: "))
+            except Exception as e:
+                print("Invalid input ", e)
+                SecondarySwitches.loan()
+            if id_class.balance >= payLoan or id_class.loan >= payLoan:
+                id_class.balance -= payLoan
+                id_class.loan -= payLoan
+            else:
+                print("Your input was either more than your available balance or more than your outstanding loan. Please try again!")
+                SecondarySwitches.loan()
+
 
 
 def instance(id):
@@ -76,11 +200,11 @@ def instance(id):
 def newAcc():
     fname = input("As you are a new customer, you should open a new account. Please input your first name: ")
     lname = input("Please input your last name: ")
-    # try:
-    initdep = int(input("Please input your initial deposit (input 0 for no deposit): "))
-    # except Exception as e:
-    #     print("Invalid input!", e)
-    #     newAcc()
+    try:
+        initdep = int(input("Please input your initial deposit (input 0 for no deposit): "))
+    except Exception as e:
+        print("Invalid input!", e)
+        newAcc()
     newid_class = NewAccount(fname, lname, initdep)
     print(f"Your initial balance is {newid_class.balance}")
     print(f"You new credit card number is: {newid_class.credit_card}")
@@ -89,8 +213,6 @@ def newAcc():
     with open('id.txt', 'a') as f:  # Saves the username and password data temporarily for testing purposes
         f.write(newid_class.id + " ")
         f.write(newid_class.password + "\n")
-    cdata = newid_class.saveToDatabase()
-    initSaveData(cdata[0], cdata[1], cdata[2], cdata[3], cdata[4], cdata[5])
     print("Please restart the program and login with your new credentials!")
     quit()
 
@@ -108,6 +230,7 @@ def login():
             instance(u_name)
             name = id_class.fname + " " + id_class.lname
             print(f"Welcome {name.capitalize()}!\n\nYou've logged in!\n")
+            print(f"Your current balance is {id_class.balance}\n")
             main()
         else:
             print("Wrong password, please try again!")
@@ -119,8 +242,8 @@ def login():
         login()
 
 
-
 def firstFunction():
+    print(f"Today's date is: {curDate}")
     new = input("Welcome to your banking service.\nAre you a new or returning customer? Please type 'New' for new or 'old' for returning customer. ")
     if new.lower() == "new":
         newAcc()
@@ -131,62 +254,7 @@ def firstFunction():
         firstFunction()
 
 
-def switch1():
-    """
-    Deposit
-    :return: New balance
-    """
-    deposit = int(input("How much would you like to deposit?"))
-    id_class.deposit(deposit)
-    main()
-    
-
-
-def switch2():
-    """
-    Withdraw
-    :return: New balance
-    """
-    withdraw = int(input("How much would you like to withdraw?"))
-    if id_class.balance > withdraw:
-        id_class.withdraw(withdraw)
-        print(f"Your new balance is {id_class.balance}")
-    else:
-        print(f"You have insufficient funds for this transaction. Your current balance is {id_class.balance}")
-    main()
-
-
-def switch3():
-    """
-    Get Credit card number
-    :return: Credit card number
-    """
-    print(f"Your credit card number is: {id_class.credit_card}")
-    main()
-
-
-def switch4():
-    """
-    Choose service
-    :return:call function of service
-    """
-
-    print("Services are still in progress! Please come back soon.")
-    main()
-
-
-def switch5():
-    """
-    Log out
-    :return: None
-    """
-    saveData(id_class.balance, id_class.id)
-    print("Thank you for using this program!")
-    quit()
-
-
 def main():
-    print(f"Your current balance is {id_class.balance}")
     to_do = input("What would you like to do? \nPress 1 to deposit.\nPress 2 to withdraw.\nPress 3 to get your credit card number.\nPress 4 for a service.\nPress 5 to log out.\n")
     def toDo(switch):
         """
@@ -194,7 +262,7 @@ def main():
         :param switch: switch number
         :return: None
         """
-        func = "switch" + str(switch)
+        func = "InitSwitches."  "switch" + str(switch)
         eval(func + "()")
     toDo(to_do)
 
